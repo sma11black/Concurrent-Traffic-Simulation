cc93074645fbcaaad448e48c8b7206c0634dfdc6
司马黑 <653014496@qq.com>
2019-10-08 05:00:48 +0800

    Implement concurrency by thread.

--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <algorithm>
 #include "TrafficObject.h"
 
 // init static variable
@@ -14,6 +15,9 @@ TrafficObject::TrafficObject()
 TrafficObject::~TrafficObject()
 {
 	// Task: Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
+	std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+		t.join();
+	});
 }
 
 void TrafficObject::setPosition(double x, double y)
